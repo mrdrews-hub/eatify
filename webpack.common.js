7a55f2b8fc532a624c0a314dd108fd4ae0e4fc73
mrdrews-hub <yancuh223@gmail.com>
2021-12-07 13:06:42 +0700

    Image Responsive

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default
+const ImageminMozJpeg = require('imagemin-mozjpeg')
 const path = require('path')
 
 module.exports = {
@@ -39,6 +41,17 @@ module.exports = {
     }),
     new ServiceWorkerWebpackPlugin({
       entry: path.resolve(__dirname, 'src/scripts/sw.js')
+    }),
+    new ImageminWebpackPlugin({
+      pngquant: {
+        quality: 50
+      },
+      plugins: [
+        ImageminMozJpeg({
+          quality: 50,
+          progressive: true
+        })
+      ]
     })
   ]
 }
