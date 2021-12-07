54204ed3e31dfbfffc1ac6d0c3109077af49e114
mrdrews-hub <yancuh223@gmail.com>
2021-12-07 15:34:45 +0700

    change global config

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
-  OFFLINE_URL: '/offline.html',
-  CACHE_NAME: new Date().toISOString(),
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
+  CACHE_NAME: 'Eatify@V.0.0.1',
   DATABASE_NAME: 'EATIFY-DATABSE',
   DATABSE_VERSION: 1,
   OBJECT_STORE_NAME: 'Resaturant',
